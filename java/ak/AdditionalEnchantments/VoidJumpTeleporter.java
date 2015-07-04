8dced22291c97f11291e5ae09d91bc10c2a17419
aksource <akira.mew@gmail.com>
2015-07-04 17:49:18 +0900

    fixed vorpal error.

--- java/ak/AdditionalEnchantments/VoidJumpTeleporter.java
+++ java/ak/AdditionalEnchantments/VoidJumpTeleporter.java
@@ -13,9 +13,9 @@ public class VoidJumpTeleporter extends Teleporter {
 	}
 
 	@Override
-	public void func_180266_a/*placeInPortal*/(Entity par1Entity, float par8) {
+	public void placeInPortal(Entity par1Entity, float par8) {
 		par1Entity.motionX = par1Entity.motionY = par1Entity.motionZ = 0.0D;
-		BlockPos blockPos = par1Entity.worldObj.func_175672_r/*getTopSolidOrLiquidBlock*/(new BlockPos(par1Entity.posX, par1Entity.posY, par1Entity.posZ));
+		BlockPos blockPos = par1Entity.worldObj.getTopSolidOrLiquidBlock(new BlockPos(par1Entity.posX, par1Entity.posY, par1Entity.posZ));
 		par1Entity.moveToBlockPosAndAngles(blockPos, 0, 0);
 		par1Entity.fallDistance = 0;
 	}
